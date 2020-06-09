dfb8f3b2823f95e79227c305e32000a78785efe4
dtsang2001 <thesang201@gmail.com>
2020-06-09 14:09:08 +0700

    Review Created

--- app/Model/Review.php
+++ app/Model/Review.php
@@ -6,6 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
+    protected $table = 'reviews';
+
+    protected $fillable = [
+    	'product_id', 'customer', 'review', 'star'
+    ];
+
     public function products(){
         return $this->belongsTo(Product::class);
     }
