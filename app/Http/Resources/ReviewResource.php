bcc14fcd80d1afe18ffc1173180375d9e5a68b0e
dtsang2001 <thesang201@gmail.com>
2020-06-09 14:33:06 +0700

    Review Update & Destroy

--- app/Http/Resources/ReviewResource.php
+++ app/Http/Resources/ReviewResource.php
@@ -15,6 +15,8 @@ class ReviewResource extends JsonResource
     public function toArray($request)
     {
         return [
+            'id' => $this->id,
+
             'customer' => $this->customer,
 
             'content' => $this->review,
